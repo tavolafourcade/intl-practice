f4a93c1aa6548af7e4195dfd4806f8d20a47581e
tavolafourcade <tavolafourcade@gmail.com>
2024-04-05 16:33:37 -0500

    UPDATE internationalization

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,6 @@
-
-'use client'
-import Home from '@/components/Home'
-
-function Page() {
-  return (
-    <Home />
-  )
+import {useTranslations} from 'next-intl';
+ 
+export default function Index() {
+  const t = useTranslations('Index');
+  return <h1>{t('title')}</h1>;
 }
-
-export default Page
